dc3dc7fc87b0c4c4ae596cff5fa1da9b3e2db26f
Jens Piegsa <piegsa@gmail.com>
2016-10-16 15:25:58 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -4,12 +4,19 @@ set -e
 
 openssl genrsa -out /certs/${KEY_NAME}.key 4096
 
-openssl req -new -nodes -sha256 \
- -subj "/C=${DE}/ST=${STATE}/L=${LOCATION}/O=${ORGANISATION}/OU=${ORGANISATION_UNIT}/CN=${COMMON_NAME}/emailAddress=${EMAIL_ADDRESS}" \
- -key  /certs/${KEY_NAME}.key \
- -out  /certs/${KEY_NAME}.csr
+if [ -z "${EMAIL_ADDRESS}" ]; then
+    openssl req -new -nodes -sha256 \
+     -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCATION}/O=${ORGANISATION}/OU=${ORGANISATION_UNIT}/CN=${COMMON_NAME}" \
+     -key  /certs/${KEY_NAME}.key \
+     -out  /certs/${KEY_NAME}.csr
+else
+    openssl req -new -nodes -sha256 \
+     -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCATION}/O=${ORGANISATION}/OU=${ORGANISATION_UNIT}/CN=${COMMON_NAME}/emailAddress=${EMAIL_ADDRESS}" \
+     -key  /certs/${KEY_NAME}.key \
+     -out  /certs/${KEY_NAME}.csr
+fi
 
-if [ -z "$CA_NAME" ]; then
+if [ -z "${CA_NAME}" ]; then
     openssl x509 -req -days ${DAYS} -set_serial 01 \
      -signkey ${KEY_NAME}.key \
      -in /certs/${KEY_NAME}.csr \
